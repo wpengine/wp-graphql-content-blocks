cdefd88cc92212bfbf2b78e62e5e1bfaf50a463e
Joe Fusco <joe.fusco@wpengine.com>
2022-11-11 13:54:09 -0500

    Scaffold e2e tests with example
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>

---
+++ tests/e2e/example.spec.js
@@ -0,0 +1,28 @@
+import { loginUser, visitAdminPage } from '@wordpress/e2e-test-utils'
+
+/**
+ * {@link https://www.wpgraphql.com/2022/02/17/adding-end-2-end-tests-to-wordpress-plugins-using-wp-env-and-wp-scripts Using wp-env and wp-scripts for WordPress plugin testing}
+ */
+describe( 'example test', () => {
+
+    it ( 'works', () => {
+        expect( true ).toBeTruthy();
+    });
+
+    it ( 'verifies the plugin is active', async () => {
+
+        // login as admin
+        await loginUser();
+
+        // visit the plugins page
+        await visitAdminPage( 'plugins.php' );
+
+        // Select the plugin based on slug and active class
+        const activePlugin = await page.$x('//tr[contains(@class, "active") and contains(@data-slug, "wpgraphql-content-blocks")]');
+
+        // assert that our plugin is active by checking the HTML
+        expect( activePlugin?.length ).toBe( 1 );
+
+    });
+
+})
