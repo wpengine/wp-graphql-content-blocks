3f4943f3e987f5128d2e4e28ff6b2a1ead7e7b23
Theofanis Despoudis <328805+theodesp@users.noreply.github.com>
2023-01-26 21:53:48 +0000

    Update BlockQueriesTest.php

--- tests/unit/BlockQueriesTest.php
+++ tests/unit/BlockQueriesTest.php
@@ -7,34 +7,34 @@ final class BlockQueriesTest extends PluginTestCase {
     public $post_id;
 
     public function setUp(): void {
-		parent::setUp();
+	parent::setUp();
         global $wpdb;
 
         $this->post_id = wp_insert_post( [
-			'post_title'   => 'Post Title',
-			'post_content' => preg_replace('/\s+/', ' ', trim('
-            <!-- wp:columns -->
-            <div class="wp-block-columns"><!-- wp:column -->
-            <div class="wp-block-column"><!-- wp:paragraph -->
-            <p>Example paragraph in Column 1</p>
-            <!-- /wp:paragraph --></div>
-            <!-- /wp:column -->
-        
-            <!-- wp:column -->
-            <div class="wp-block-column"><!-- wp:paragraph -->
-            <p>Example paragraph in Column 2</p>
-            <!-- /wp:paragraph --></div>
-            <!-- /wp:column --></div>
-            <!-- /wp:columns -->')),
-			'post_status'  => 'publish'
+		'post_title'   => 'Post Title',
+		'post_content' => preg_replace('/\s+/', ' ', trim('
+			    <!-- wp:columns -->
+			    <div class="wp-block-columns"><!-- wp:column -->
+			    <div class="wp-block-column"><!-- wp:paragraph -->
+			    <p>Example paragraph in Column 1</p>
+			    <!-- /wp:paragraph --></div>
+			    <!-- /wp:column -->
+
+			    <!-- wp:column -->
+			    <div class="wp-block-column"><!-- wp:paragraph -->
+			    <p>Example paragraph in Column 2</p>
+			    <!-- /wp:paragraph --></div>
+			    <!-- /wp:column --></div>
+			    <!-- /wp:columns -->')),
+		'post_status'  => 'publish'
 		] );
 	}
 
-	public function tearDown(): void {
-		// your tear down methods here
-		parent::tearDown();
-        wp_delete_post( $this->post_id, true );
-	}
+    public function tearDown(): void {
+    	// your tear down methods here
+	parent::tearDown();
+       	wp_delete_post( $this->post_id, true );
+    }
 
     public function test_retrieve_non_flatten_content_blocks() {
         $query = '
@@ -42,22 +42,21 @@ final class BlockQueriesTest extends PluginTestCase {
 			posts(first: 1) {
 				nodes {
 					databaseId
-                    contentBlocks {
-                        name
-                    }
+                    			contentBlocks {
+                        			name
+                    			}
 				}
 			}
 		}
 		';
-		$actual = graphql( [ 'query' => $query ] );
+	$actual = graphql( [ 'query' => $query ] );
         $node = $actual['data']['posts']['nodes'][0];
 
-		// Verify that the ID of the first post matches the one we just created.
-		$this->assertEquals( $this->post_id, $node['databaseId'] );
+	// Verify that the ID of the first post matches the one we just created.
+	$this->assertEquals( $this->post_id, $node['databaseId'] );
 
-		// There should be only one block using that query when not using flat: true
+	// There should be only one block using that query when not using flat: true
         $this->assertEquals(count($node['contentBlocks']), 1);
-
         $this->assertEquals($node['contentBlocks'][0]['name'], 'core/columns');
     }
 
@@ -67,22 +66,22 @@ final class BlockQueriesTest extends PluginTestCase {
 			posts(first: 1) {
 				nodes {
 					databaseId
-                    contentBlocks(flat: true) {
-                        name
-                        parentId
-                    }
+                    			contentBlocks(flat: true) {
+                        			name
+                        			parentId
+                    			}
 				}
 			}
 		}
 		';
 
-		$actual = graphql( [ 'query' => $query ] );
+	$actual = graphql( [ 'query' => $query ] );
         $node = $actual['data']['posts']['nodes'][0];
         
-		// Verify that the ID of the first post matches the one we just created.
-		$this->assertEquals( $this->post_id, $node['databaseId'] );
+	// Verify that the ID of the first post matches the one we just created.
+	$this->assertEquals( $this->post_id, $node['databaseId'] );
 
-		// There should more than one block using that query when using using flat: true
+	// There should more than one block using that query when using flat: true
         $this->assertEquals(count($node['contentBlocks']), 5);
 
         $this->assertEquals($node['contentBlocks'][0]['name'], 'core/columns');
