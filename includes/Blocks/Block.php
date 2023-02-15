128f6b180ccbec3811416bbb212c1071c406fd4d
Matthew Wright <matthew.wright@wpengine.com>
2023-02-15 11:28:47 -0600

    removed unneeded code block

--- includes/Blocks/Block.php
+++ includes/Blocks/Block.php
@@ -159,9 +159,6 @@ class Block {
 					'type'        => $graphql_type,
 					'description' => __( sprintf( 'The "%1$s" field on the "%2$s" block', $attribute_name, $this->type_name ), 'wp-graphql' ),
 					'resolve'     => function ( $block, $args, $context, $info ) use ( $attribute_name, $attribute_config ) {
-						if ( $graphql_type === 'object' ) {
-							// isset( $block['attrs']['data'] ) ? json_decode($block['attrs']['data']) : array().
-						}
 						return $this->resolve_block_attributes( $block, $attribute_name, $attribute_config );
 					},
 				);
