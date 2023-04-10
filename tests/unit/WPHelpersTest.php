0d5f3f748d09fd4f4f44a1022d5a47bdf128da07
Jason Bahl <jasonbahl@mac.com>
2023-04-10 15:12:23 -0600

    - update the test to better identify if the WPHelper::get_supported_post_types() function returns expected results. Was returning unexpected results before.

--- tests/unit/WPHelpersTest.php
+++ tests/unit/WPHelpersTest.php
@@ -18,21 +18,36 @@ final class WPHelpersTest extends PluginTestCase {
 			)
 		);
 		register_post_type(
-			'recipe',
+			'blocks_enabled',
 			array(
 				'show_in_graphql'     => true,
-				'graphql_single_name' => 'recipe',
-				'graphql_plural_name' => 'recipes',
+				'graphql_single_name' => 'blocksEnabled',
+				'graphql_plural_name' => 'blocksEnabled',
 				'supports'            => array( 'title', 'editor', 'author', 'thumbnail' ),
 				'public'              => true,
+				'show_in_rest'        => true
 			)
 		);
+
+		register_post_type(
+			'blocks_disabled',
+			array(
+				'show_in_graphql'     => true,
+				'graphql_single_name' => 'blocksDisabled',
+				'graphql_plural_name' => 'blocksDisabled',
+				'supports'            => array( 'title', 'editor', 'author', 'thumbnail' ),
+				'public'              => true,
+				'show_in_rest'        => false // post types that support the editor but do not show in rest will be prevented from using the Block editor
+			)
+		);
+
 		\WPGraphQL::clear_schema();
 	}
 
 	public function tearDown(): void {
 		unregister_post_type( 'faq' );
-		unregister_post_type( 'recipe' );
+		unregister_post_type( 'blocks_enabled' );
+		unregister_post_type( 'blocks_disabled' );
 		\WPGraphQL::clear_schema();
 		parent::tearDown();
 	}
@@ -40,11 +55,14 @@ final class WPHelpersTest extends PluginTestCase {
 	 * @covers WPHelpers::get_supported_post_types
 	 */
 	public function test_get_supported_post_types() {
-		$expected_post_types = array(
-			get_post_type_object( 'post' ),
-			get_post_type_object( 'page' ),
-			get_post_type_object( 'recipe' ),
-		);
-		$this->assertEquals( WPHelpers::get_supported_post_types(), $expected_post_types );
+
+		$supported_post_types = WPHelpers::get_supported_post_types();
+
+		$this->assertContains( get_post_type_object( 'post' ), $supported_post_types );
+		$this->assertContains( get_post_type_object( 'page' ), $supported_post_types );
+		$this->assertContains( get_post_type_object( 'blocks_enabled' ), $supported_post_types );
+		$this->assertNotContains( get_post_type_object( 'blocks_disabled' ), $supported_post_types );
+
+
 	}
 }
