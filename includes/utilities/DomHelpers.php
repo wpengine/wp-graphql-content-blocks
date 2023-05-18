14e7fd2832e87caa7872acf0a6e8015ccde0eabf
John Parris <john.parris@wpengine.com>
2023-05-18 15:48:00 -0400

    chore: coding standards

--- includes/utilities/DomHelpers.php
+++ includes/utilities/DomHelpers.php
@@ -19,20 +19,20 @@ final class DOMHelpers {
 	 * @param string $html The HTML string to parse.
 	 * @param string $selector The selector to use.
 	 * @param string $attribute The attribute to extract.
-	 * @param mixed  $default The default value to return if the selector is not found.
+	 * @param mixed  $default_value The default value to return if the selector is not found.
 	 *
 	 * @return string|null extracted attribute
 	 */
-	public static function parseAttribute( $html, $selector, $attribute, $default = null ) {
+	public static function parseAttribute( $html, $selector, $attribute, $default_value = null ) {
 		$value = null;
 		$doc   = new Document();
 		$doc->loadHTML( $html );
 		if ( '*' === $selector ) {
 			$selector = '*[' . $attribute . ']';
 		}
-		$node    = $doc->find( $selector );
-		$default = isset( $default ) ? $default : null;
-		$value   = ( ! empty( $node ) && isset( $node[0] ) ) ? $node[0]->getAttribute( $attribute ) : $default;
+		$node          = $doc->find( $selector );
+		$default_value = isset( $default_value ) ? $default_value : null;
+		$value         = ( ! empty( $node ) && isset( $node[0] ) ) ? $node[0]->getAttribute( $attribute ) : $default_value;
 		return $value;
 	}
 
@@ -62,17 +62,17 @@ final class DOMHelpers {
 	/**
 	 * Parses the given HTML string to extract the innerHTML contents.
 	 *
-	 * @param string  $html The HTML string to parse.
-	 * @param string  $selector The selector to use.
-	 * @param default $default The default value to return if the selector is not found.
+	 * @param string $html The HTML string to parse.
+	 * @param string $selector The selector to use.
+	 * @param mixed  $default_value The default value to return if the selector is not found.
 	 *
 	 * @return string|null extracted innerHTML of selector
 	 */
-	public static function parseHTML( $html, $selector, $default = null ) {
+	public static function parseHTML( $html, $selector, $default_value = null ) {
 		$doc = new Document();
 		$doc->loadHTML( $html );
 		$node       = $doc->find( $selector );
-		$inner_html = isset( $default ) ? $default : '';
+		$inner_html = isset( $default_value ) ? $default_value : '';
 
 		foreach ( $node as $elem ) {
 			$inner_html .= $elem->innerHTML();
