91a0b489e6691ce5d8e58bc64fefe43de381e075
Dovid Levine <david@axepress.dev>
2023-05-23 16:13:16 +0300

    chore: configure PHPStan static analysis (#87)
    * chore: configure PHPStan static analysis
    
    * chore: exclude `/phpstan/` from CS ruleset
    
    ---------
    
    Co-authored-by: John Parris <john.parris@wpengine.com>

---
+++ phpstan/constants.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Constants defined in this file are to help phpstan analyze code where constants outside the plugin (WordPress core constants, etc) are being used.
+ *
+ * @package WPGraphQL\ContentBlocks
+ */
+
+define( 'WPGRAPHQL_CONTENT_BLOCKS_DIR', '' );
+define( 'WPGRAPHQL_CONTENT_BLOCKS_VERSION', '0.2.1' );
+define( 'WPGRAPHQL_CONTENT_BLOCKS_AUTOLOAD', true );
+define( 'WPGRAPHQL_CONTENT_BLOCKS_PLUGIN_FILE', 'wp-graphql-content-blocks.php' );
+define( 'WPGRAPHQL_CONTENT_BLOCKS_MIN_PHP_VERSION', '7.4' );
