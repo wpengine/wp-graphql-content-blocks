cc6dc3806b72ad1e3162dcbbad4a62d50841cbbe
theodesp <328805+theodesp@users.noreply.github.com>
2023-06-23 12:43:14 +0100

    Feat: Add CoreHeading css classNames

---
+++ includes/Blocks/CoreHeading.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Core Heading Block
+ *
+ * @package WPGraphQL\ContentBlocks\Blocks
+ */
+
+namespace WPGraphQL\ContentBlocks\Blocks;
+
+/**
+ * Class CoreHeading
+ */
+class CoreHeading extends Block {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @var array|null
+	 */
+	protected ?array $additional_block_attributes = array(
+		'cssClassName' => array(
+			'type'      => 'string',
+			'selector'  => 'h1, h2, h3, h4, h5, h6',
+			'source'    => 'attribute',
+			'attribute' => 'class',
+		),
+	);
+}
