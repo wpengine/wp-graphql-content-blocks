3173f2837b20f2aa0f931972e50acdc6c2bf8800
John Parris <john.parris@wpengine.com>
2024-01-19 11:35:06 -0500

    test: confirm update callbacks are attached

---
+++ tests/unit/updates/test-update-callbacks.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Class TestUpdateCallbacks
+ */
+class TestUpdateCallbacks extends WP_UnitTestCase {
+	public function test_pre_set_site_transient_update_plugins_has_filter_added(): void {
+		self::assertSame( 10, has_action( 'pre_set_site_transient_update_plugins', 'WPGraphQL\ContentBlocks\PluginUpdater\check_for_plugin_updates' ) );
+	}
+
+	public function test_plugins_api_has_filter_added(): void {
+		self::assertSame( 10, has_action( 'plugins_api', 'WPGraphQL\ContentBlocks\PluginUpdater\custom_plugin_api_request' ) );
+	}
+
+	public function test_admin_notices_has_actions_added(): void {
+		self::assertSame( 10, has_action( 'admin_notices', 'WPGraphQL\ContentBlocks\PluginUpdater\delegate_plugin_row_notice' ) );
+		self::assertSame( 10, has_action( 'admin_notices', 'WPGraphQL\ContentBlocks\PluginUpdater\display_update_page_notice' ) );
+	}
+}
