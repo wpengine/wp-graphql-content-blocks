6241c4e23428e5c44fa43731e284ab220a8af6a1
Dovid Levine <david@axepress.dev>
2024-08-07 13:14:25 +0300

    fix: prevent fatal errors with DOMHelpers methods by improving typesafety and returning early (#257)
    * chore: update Composer dev-deps to latest
    
    * chore: cleanup EOL whitespace [phpcbf]
    
    * chore: update phpstan.neon config
    
    * fix: `Block::get_attribute_type()` param type and usage.
    
    * fix: improve typesafety in DOMHelpers

--- tests/unit/DOMHelpersTest.php
+++ tests/unit/DOMHelpersTest.php
@@ -19,27 +19,28 @@ final class DOMHelpersTest extends PluginTestCase {
 		$id_attribute = 'id';
 
 		// $html
-		$this->assertNull( DOMHelpers::parseAttribute( $html, $no_existent_selector, $data_attribute ) );
-		$this->assertEquals( DOMHelpers::parseAttribute( $html, $no_existent_selector, $data_attribute, 'Bar' ), 'Bar' );
-		$this->assertEquals( DOMHelpers::parseAttribute( $html, $id_selector, $data_attribute ), 'foo-data' );
-		$this->assertEquals( DOMHelpers::parseAttribute( $html, $id_selector, $class_attribute ), 'foo-class' );
-		$this->assertEquals( DOMHelpers::parseAttribute( $html, $id_selector, $id_attribute ), 'foo-id' );
-		$this->assertEquals( DOMHelpers::parseAttribute( $html, $class_selector, $data_attribute ), 'foo-data' );
-		$this->assertEquals( DOMHelpers::parseAttribute( $html, $class_selector, $class_attribute ), 'foo-class' );
-		$this->assertEquals( DOMHelpers::parseAttribute( $html, $class_selector, $id_attribute ), 'foo-id' );
-		$this->assertEquals( DOMHelpers::parseAttribute( $html, $element_selector, $data_attribute ), 'foo-data' );
-		$this->assertEquals( DOMHelpers::parseAttribute( $html, $element_selector, $class_attribute ), 'foo-class' );
-		$this->assertEquals( DOMHelpers::parseAttribute( $html, $element_selector, $id_attribute ), 'foo-id' );
+		$this->assertNull( DOMHelpers::parse_attribute( '', $no_existent_selector, $data_attribute ) );
+		$this->assertNull( DOMHelpers::parse_attribute( $html, $no_existent_selector, $data_attribute ) );
+		$this->assertEquals( DOMHelpers::parse_attribute( $html, $no_existent_selector, $data_attribute, 'Bar' ), 'Bar' );
+		$this->assertEquals( DOMHelpers::parse_attribute( $html, $id_selector, $data_attribute ), 'foo-data' );
+		$this->assertEquals( DOMHelpers::parse_attribute( $html, $id_selector, $class_attribute ), 'foo-class' );
+		$this->assertEquals( DOMHelpers::parse_attribute( $html, $id_selector, $id_attribute ), 'foo-id' );
+		$this->assertEquals( DOMHelpers::parse_attribute( $html, $class_selector, $data_attribute ), 'foo-data' );
+		$this->assertEquals( DOMHelpers::parse_attribute( $html, $class_selector, $class_attribute ), 'foo-class' );
+		$this->assertEquals( DOMHelpers::parse_attribute( $html, $class_selector, $id_attribute ), 'foo-id' );
+		$this->assertEquals( DOMHelpers::parse_attribute( $html, $element_selector, $data_attribute ), 'foo-data' );
+		$this->assertEquals( DOMHelpers::parse_attribute( $html, $element_selector, $class_attribute ), 'foo-class' );
+		$this->assertEquals( DOMHelpers::parse_attribute( $html, $element_selector, $id_attribute ), 'foo-id' );
 
 		// $html2
-		$this->assertEquals( 'center', DOMHelpers::parseAttribute( $html2, '*', 'data-align' ) );
-		$this->assertEquals( 'right', DOMHelpers::parseAttribute( $html2, '.has-text-align-right', 'data-align' ) );
-		$this->assertNull( DOMHelpers::parseAttribute( $html2, '.non-existent-class', 'data-align' ) );
-		$this->assertEquals( 'default', DOMHelpers::parseAttribute( $html2, '.non-existent-class', 'data-align', 'default' ) );
+		$this->assertEquals( 'center', DOMHelpers::parse_attribute( $html2, '*', 'data-align' ) );
+		$this->assertEquals( 'right', DOMHelpers::parse_attribute( $html2, '.has-text-align-right', 'data-align' ) );
+		$this->assertNull( DOMHelpers::parse_attribute( $html2, '.non-existent-class', 'data-align' ) );
+		$this->assertEquals( 'default', DOMHelpers::parse_attribute( $html2, '.non-existent-class', 'data-align', 'default' ) );
 
 		// $htm3
-		$this->assertEquals( 'left', DOMHelpers::parseAttribute( $html3, 'span', 'data-align' ) );
-		$this->assertEquals( 'left', DOMHelpers::parseAttribute( $html3, '*', 'data-align' ) );
+		$this->assertEquals( 'left', DOMHelpers::parse_attribute( $html3, 'span', 'data-align' ) );
+		$this->assertEquals( 'left', DOMHelpers::parse_attribute( $html3, '*', 'data-align' ) );
 	}
 
 	public function testParseHTML(): void {
@@ -49,11 +50,12 @@ final class DOMHelpersTest extends PluginTestCase {
 		$class_selector = '.foo-class';
 		$element_selector = 'p';
 
-		$this->assertEmpty( DOMHelpers::parseHTML( $html, $no_existent_selector ) );
-		$this->assertEquals( DOMHelpers::parseHTML( $html, $no_existent_selector, 'Bar' ), 'Bar' );
-		$this->assertEquals( DOMHelpers::parseHTML( $html, $id_selector ), '<span>Bar</span>' );
-		$this->assertEquals( DOMHelpers::parseHTML( $html, $class_selector ), '<span>Bar</span>' );
-		$this->assertEquals( DOMHelpers::parseHTML( $html, $element_selector ), '<span>Bar</span>' );
+		$this->assertNull( DOMHelpers::parse_html( '', $no_existent_selector ) );
+		$this->assertEmpty( DOMHelpers::parse_html( $html, $no_existent_selector ) );
+		$this->assertEquals( DOMHelpers::parse_html( $html, $no_existent_selector, 'Bar' ), 'Bar' );
+		$this->assertEquals( DOMHelpers::parse_html( $html, $id_selector ), '<span>Bar</span>' );
+		$this->assertEquals( DOMHelpers::parse_html( $html, $class_selector ), '<span>Bar</span>' );
+		$this->assertEquals( DOMHelpers::parse_html( $html, $element_selector ), '<span>Bar</span>' );
 	}
 
 	public function testGetElementsFromHTML(): void {
@@ -61,8 +63,9 @@ final class DOMHelpersTest extends PluginTestCase {
 		$element_selector = 'p';
 		$no_existent_selector = 'span';
 
-		$this->assertEquals( DOMHelpers::getElementsFromHTML( $html, $element_selector ), '<p>First paragraph</p><p>Second paragraph</p>' );
-		$this->assertEmpty( DOMHelpers::getElementsFromHTML( $html, $no_existent_selector ) );
+		$this->assertNull( DOMHelpers::get_elements_from_html( '', $no_existent_selector ) );
+		$this->assertEquals( DOMHelpers::get_elements_from_html( $html, $element_selector ), '<p>First paragraph</p><p>Second paragraph</p>' );
+		$this->assertEmpty( DOMHelpers::get_elements_from_html( $html, $no_existent_selector ) );
 	}
 
 	public function getTextFromSelector(): void {
@@ -79,6 +82,6 @@ final class DOMHelpersTest extends PluginTestCase {
 
 		$this->assertEquals( DOMHelpers::getTextFromSelector( $html, $p_element ), 'First paragraph' );
 		$this->assertEquals( DOMHelpers::getTextFromSelector( $html, $div_element ), 'My div' );
-		$this->assertEmpty( DOMHelpers::getElementsFromHTML( $html, $no_existent_selector ) );
+		$this->assertEmpty( DOMHelpers::get_elements_from_html( $html, $no_existent_selector ) );
 	}
 }
