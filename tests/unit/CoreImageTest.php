96cf870da8cc09e1da49a374aac86b3473540377
Theo <328805+theodesp@users.noreply.github.com>
2024-08-14 11:54:38 +0100

    chore: debug unit test

--- tests/unit/CoreImageTest.php
+++ tests/unit/CoreImageTest.php
@@ -109,6 +109,7 @@ final class CoreImageTest extends PluginTestCase {
 		  }
 		';
 		$actual = graphql( array( 'query' => $query ) );
+		print_r($actual);
 		$node   = $actual['data']['posts']['nodes'][0];
 		
 		// Verify that the ID of the first post matches the one we just created.
