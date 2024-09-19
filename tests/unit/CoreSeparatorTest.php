5c924772ad1878d58f223c3e6ae0fccb949bb689
Ta5r <srv.tanay@gmail.com>
2024-09-19 20:42:31 +0530

    chore : test cleanup

--- tests/unit/CoreSeparatorTest.php
+++ tests/unit/CoreSeparatorTest.php
@@ -34,47 +34,49 @@ final class CoreSeparatorTest extends PluginTestCase {
 
 	public function query(): string {
 		return '
-            fragment CoreSeparatorBlockFragment on CoreSeparator {
-                attributes {
-                    align
-                    anchor
-                    backgroundColor
-                    className
-                    cssClassName
-                    gradient
-                    opacity
-                    style
-                }
-            }
+			fragment CoreSeparatorBlockFragment on CoreSeparator {
+				attributes {
+					align
+					anchor
+					backgroundColor
+					className
+					cssClassName
+					gradient
+					lock
+					# metadata
+					opacity
+					style
+				}
+			}
 
-            query Post( $id: ID! ) {
-                post(id: $id, idType: DATABASE_ID) {
-                    databaseId
-                    editorBlocks {
-                        apiVersion
-                        blockEditorCategoryName
-                        clientId
-                        cssClassNames
-                        innerBlocks {
-                            name
-                        }
-                        isDynamic
-                        name
-                        parentClientId
-                        renderedHtml
-                        ...CoreSeparatorBlockFragment
-                    }
-                }
-            }
-        ';
+			query Post( $id: ID! ) {
+				post(id: $id, idType: DATABASE_ID) {
+					databaseId
+					editorBlocks {
+						apiVersion
+						blockEditorCategoryName
+						clientId
+						cssClassNames
+						innerBlocks {
+							name
+						}
+						isDynamic
+						name
+						parentClientId
+						renderedHtml
+						...CoreSeparatorBlockFragment
+					}
+				}
+			}
+		';
 	}
 
-	public function test_retrieve_core_separator_attributes() {
+	public function test_core_separator_fields_and_attributes() {
 		$block_content = '
-        <!-- wp:separator {"align":"wide"} -->
-        <hr class="wp-block-separator alignwide has-alpha-channel-opacity"/>
-        <!-- /wp:separator -->
-        ';
+		<!-- wp:separator {"lock":{"move":true,"remove":true},"align":"wide","className":"is-style-dots","style":{"color":{"gradient":"linear-gradient(135deg,rgb(6,147,227) 1%,rgb(155,81,224) 100%)"}}} -->
+		<hr class="wp-block-separator alignwide has-alpha-channel-opacity"/>
+		<!-- /wp:separator -->
+		';
 
 		// Set post content.
 		wp_update_post(
@@ -100,30 +102,42 @@ final class CoreSeparatorTest extends PluginTestCase {
 
 		// There should be only one block using that query when not using flat: true
 		$this->assertEquals( 1, count( $actual['data']['post']['editorBlocks'] ) );
-		$this->assertEquals( 'core/separator', $actual['data']['post']['editorBlocks'][0]['name'], 'The block name should be core/separator' );
+
+		$block = $actual['data']['post']['editorBlocks'][0];
+
+		// Verify the block data.
+		$this->assertNotEmpty( $block['apiVersion'], 'The apiVersion should be present' );
+		$this->assertEquals( 'design', $block['blockEditorCategoryName'], 'The blockEditorCategoryName should be text' );
+		$this->assertNotEmpty( $block['clientId'], 'The clientId should be present' );
+		$this->assertNotEmpty( $block['cssClassNames'], 'There should be cssClassNames' );
+		$this->assertEmpty( $block['innerBlocks'], 'There should be no inner blocks' );
+		$this->assertEquals( 'core/separator', $block['name'], 'The block name should be core/separator' );
+		$this->assertEmpty( $block['parentClientId'], 'There should be no parentClientId' );
+		$this->assertNotEmpty( $block['renderedHtml'], 'The renderedHtml should be present' );
 
 		// Verify the attributes.
 		$this->assertEquals(
 			[
-				'cssClassName'    => 'wp-block-separator alignwide has-alpha-channel-opacity',
 				'align'           => 'wide',
 				'anchor'          => null,
 				'backgroundColor' => null,
-				'className'       => null,
-				'gradient'        => null,
+				'className'       => 'is-style-dots',
+				'cssClassName'    => 'wp-block-separator alignwide has-alpha-channel-opacity',
+				'gradient'        => null, // @todo: getting null returned here, but should be the gradient value.
 				'opacity'         => 'alpha-channel',
-				'style'           => null,
+				'style'           => '{"color":{"gradient":"linear-gradient(135deg,rgb(6,147,227) 1%,rgb(155,81,224) 100%)"}}',
+				'lock'            => '{"move":true,"remove":true}',
 			],
-			$actual['data']['post']['editorBlocks'][0]['attributes'],
+			$block['attributes'],
 		);
 	}
 
-	public function test_retrieve_core_separator_attributes_two() {
+	public function test_core_separator_untested_attributes() {
 		$block_content = '
-            <!-- wp:separator -->
-            <hr class="wp-block-separator has-alpha-channel-opacity"/>
-            <!-- /wp:separator -->
-        ';
+			<!-- wp:separator {"backgroundColor":"accent-4"}  -->
+			<hr class="wp-block-separator has-text-color has-accent-4-color has-alpha-channel-opacity has-accent-4-background-color has-background" id="test-anchor"/>
+			<!-- /wp:separator -->
+		';
 
 		// Set post content.
 		wp_update_post(
@@ -154,14 +168,15 @@ final class CoreSeparatorTest extends PluginTestCase {
 		// Verify the attributes.
 		$this->assertEquals(
 			[
-				'cssClassName'    => 'wp-block-separator has-alpha-channel-opacity',
 				'align'           => null,
-				'anchor'          => null,
-				'backgroundColor' => null,
+				'anchor'          => 'test-anchor',
+				'backgroundColor' => 'accent-4',
 				'className'       => null,
+				'cssClassName'    => 'wp-block-separator has-text-color has-accent-4-color has-alpha-channel-opacity has-accent-4-background-color has-background',
 				'gradient'        => null,
 				'opacity'         => 'alpha-channel',
 				'style'           => null,
+				'lock'            => null,
 			],
 			$actual['data']['post']['editorBlocks'][0]['attributes'],
 		);
