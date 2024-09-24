a5c7b5d1e4d9b78361501f902caea76c8b8d9320
Ashutosh Gautam <ashutoshgautam10b11@gmail.com>
2024-09-24 17:11:52 +0530

    temp: log breaks

--- tests/unit/CoreParagraphTest.php
+++ tests/unit/CoreParagraphTest.php
@@ -199,6 +199,7 @@ final class CoreParagraphTest extends PluginTestCase {
 		];
 
 		$actual = graphql( compact( 'query', 'variables' ) );
+		error_log( print_r( $actual, true ) );
 
 		$this->assertArrayNotHasKey( 'errors', $actual, 'There should not be any errors' );
 		$this->assertArrayHasKey( 'data', $actual, 'The data key should be present' );
