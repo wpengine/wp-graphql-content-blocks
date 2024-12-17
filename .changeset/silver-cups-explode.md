ffb6c83d5da5bf9162c5a0fc32cc3689c79f009b
Dovid Levine <david@axepress.dev>
2024-12-17 20:09:54 +0200

    feat: add support for resolving Template Part blocks
    Co-authored-by: Ta5r <srv.tanay@gmail.com>

---
+++ .changeset/silver-cups-explode.md
@@ -0,0 +1,5 @@
+---
+"@wpengine/wp-graphql-content-blocks": minor
+---
+
+feat: add support for resolving Template Part blocks
--- includes/Data/ContentBlocksResolver.php
+++ includes/Data/ContentBlocksResolver.php
@@ -148,7 +148,7 @@ final class ContentBlocksResolver {
 		$block['clientId'] = uniqid();
 
 		// @todo apply more hydrations.
-
+		$block = self::populate_template_part_inner_blocks( $block );
 		$block = self::populate_reusable_blocks( $block );
 
 		$block = self::populate_pattern_inner_blocks( $block );
@@ -181,6 +181,36 @@ final class ContentBlocksResolver {
 		$stripped = preg_replace( '/<!--(.*)-->/Uis', '', $block['innerHTML'] );
 
 		return empty( trim( $stripped ?? '' ) );
+	}
+
+	/**
+	 * Populates the innerBlocks of a template part block with the blocks from the template part.
+	 *
+	 * @param array<string,mixed> $block The block to populate.
+	 *
+	 * @return array<string,mixed> The populated block.
+	 */
+	private static function populate_template_part_inner_blocks( array $block ): array {
+		// Bail if not WP 5.8 or later.
+		if ( ! function_exists( 'get_block_templates' ) ) {
+			return $block;
+		}
+
+		if ( 'core/template-part' !== $block['blockName'] || ! isset( $block['attrs']['slug'] ) ) {
+			return $block;
+		}
+
+		$matching_templates = get_block_templates( [ 'slug__in' => [ $block['attrs']['slug'] ] ], 'wp_template_part' );
+
+		$template_blocks = ! empty( $matching_templates[0]->content ) ? self::parse_blocks( $matching_templates[0]->content ) : null;
+
+		if ( empty( $template_blocks ) ) {
+			return $block;
+		}
+
+		$block['innerBlocks'] = $template_blocks;
+
+		return $block;
 	}
 
 	/**
