84a65bb2a933e4ac7b7d1ea2c1043dbdf237ca6a
Colin Murphy <colinmurphy@users.noreply.github.com>
2025-02-27 18:46:27 +0000

    bug: Fixes WP GraphQL fatal deactivation error (#355)
    * Fixes #350
    
    Updated semver plugin to version 3
    
    Added wp-graphql as a required plugin to ensure you cannot enable the plguin without WPGraphQL enabled.
    
    * Added tested to and requires for WP GraphQL as per suggestion from @justlevine
    
    * Added Changeset
    
    * PHPCS fix. Added line break.

--- wp-graphql-content-blocks.php
+++ wp-graphql-content-blocks.php
@@ -11,6 +11,9 @@
  * Version: 4.8.0
  * Requires PHP: 7.4
  * Requires at least: 5.7
+ * Requires Plugins: wp-graphql
+ * WPGraphQL requires at least: 1.14.5
+ * WPGraphQL tested up to: 2.0.0
  *
  * @package WPGraphQL\ContentBlocks
  */
